75078dd4eceb123275d484ad47aa1febd28ce859
prasolova.nadya@mail.ru <prasolova.nadya@mail.ru>
2022-08-09 22:10:13 +0500

    Functions

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,6 +1,23 @@
 ﻿#include<iostream>
 using namespace std;
 
+int Add(int a, int b);
+int Sub(int a, int b);
+int Mul(int a, int b);
+double Div(int a, int b);
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b;
+	cout << "Введите дв числа: "; cin >> a >> b;
+	int c = Add(a, b);
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Sub(a, b) << endl;
+	cout << a << " * " << b << " = " << Mul(a, b) << endl;
+	cout << a << " / " << b << " = " << Div(a, b) << endl;
+}
+
 int Add(int a, int b)
 {
 	//Addition - Сложение
@@ -17,19 +34,9 @@ int Mul(int a, int b)
 	//Multiplication - Умножение
 	return a * b;
 }
-int Div(int a, int b)
+double Div(int a, int b)
 {
 	//Division - Деление
-	return a / b;
+	return (double)a / b;
 }
 
-void main()
-{
-	setlocale(LC_ALL, "Russian");
-	int a, b;
-	cout << "Введите дв числа: "; cin >> a >> b;
-	int c = Add(a, b);
-	cout << a << " + " << b << " = " << c << endl;
-	cout << a << " - " << b << " = " << Sub(a, b) << endl;
-
-}
