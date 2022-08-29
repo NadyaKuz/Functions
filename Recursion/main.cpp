146ea066ae26b51e3e7716994004f4b6c5a7d110
prasolova.nadya@mail.ru <prasolova.nadya@mail.ru>
2022-08-29 10:37:01 +0500

    Recursion DONE

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -4,7 +4,10 @@ using namespace std;
 void elevator(int floor);
 int fact(int N);
 double power(double a, int n);
+void Fibonacci(int n, int a = 0, int b = 1);
 //#define ELEVATOR
+//#define POWER
+#define FIBONACCI
 
 #ifdef ELEVATOR
 
@@ -37,17 +40,26 @@ void main()
  cout << "Введите целое число для вычисления факториала: "; cin >> N;
  cout << fact(N) << endl;
 
-	int a;
+#ifdef POWER
+ int a;
+ int n;
+ cout << "Введите основание степени: "; cin >> a;
+ cout << "Введите показатель степени: "; cin >> n;
+ cout << power(a, n) << endl;
+#endif // POWER
+
+#ifdef FIBONACCI
 	int n;
-	cout << "Введите основание степени: "; cin >> a;
-	cout << "Введите показатель степени: "; cin >> n;
-	cout << power(a, n) << endl;
+	cout << "Введите число фибоначи: "; cin >> n;
+	Fibonacci(n);
+#endif // FIBONACCI
+
 }
 int fact(int N)
 {
 	if (N == 0)
 	{
-		return 0;
+		return 1;
 	}
 	if (N == 1)
 	{
@@ -65,4 +77,12 @@ double power(double a, int n)
 	{
 		return a * power(a, n - 1);
 	}
+	//else return 1 / a * power(a, n + 1);
+	else return power(1 / a, - n);
+}
+void Fibonacci(int n, int a, int b)
+{
+	if (a > n)return;
+	cout << a << "\t";
+	Fibonacci(n, b, a + b);
 }
